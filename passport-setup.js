bb84a8bc13481f7777cb05c8cc4679245b5cad1d
Sunnybokhari <sunnybokhari@hotmail.com>
2024-01-06 00:33:04 +0000

    f

--- passport-setup.js
+++ passport-setup.js
@@ -6,7 +6,7 @@ passport.use(
     new GoogleStrategy({
         clientID: process.env.OAUTH_CLIENT_ID,
         clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        callbackURL: '/auth/google/redirect'
+        callbackURL: 'https://whispering-bayou-52899-2488ba1a59ea.herokuapp.com//auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
         done(null, { email: profile.emails[0].value });
 
